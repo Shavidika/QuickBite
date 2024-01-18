090f90c095f8e4d0ce8feccaebce2497e3172433
Supuni Jayasinghe <139542877+SupuniJayasinghe@users.noreply.github.com>
2024-01-18 18:10:03 +0530

    order page

--- lib/src/App.dart
+++ lib/src/App.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:quickbite/src/homeScreen.dart';
+import 'package:quickbite/src/pages/order_page.dart';
 import 'package:quickbite/src/screens/main_screen.dart';
+import 'package:quickbite/src/widgets/order_card.dart';
 
 class App extends StatelessWidget {
   @override
